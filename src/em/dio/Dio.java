d0d6bed58133d6f9165f6e4f243d45ddd0f92115
ikram <ikrmo959@gmail.com>
2018-05-17 10:46:05 +0200

    project functions done

--- src/em/dio/Dio.java
+++ src/em/dio/Dio.java
@@ -2,12 +2,26 @@ package em.dio;
 
 import java.util.List;
 
+import em.model.Project;
 import em.model.User;
+
 
 public interface Dio {
 	public User getUser(int id);
 	public void addUser(User user);
 	public void deleteUser(int id);
-	public void updateUser(User user);
+	public void updateUser(User User);
 	public List<User> getUsers();
+	
+	
+// for project 
+// ikram
+	public Project getProject(int id);
+	public void addProject(Project Project);
+	public void deleteProject(int id);
+	public void updateProject(Project Project);
+	public List<Project> getProjects();
+//end ikram	
 }
+
+
